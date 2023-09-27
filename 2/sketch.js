9035271ee47cc8a8692ca2ef26b6d4228fc4a35b
Ayushi Shah <as17726@nyu.edu>
2023-09-27 16:27:52 -0400

    2 - time

---
+++ 2/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  fill(255);
+  print(millis());
+}
+
+
+function draw() {
+  background ('darkblue');
+  let secondsNow = second();
+  let myDiameter = map(secondsNow, 0, 59, 100, height); 
+  ellipse(width/2, height/2, myDiameter,myDiameter); 
+}
